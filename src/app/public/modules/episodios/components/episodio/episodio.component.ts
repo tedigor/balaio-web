65e224da6f1cb14434cc7ac1bd4cc0edc5b27527
Ted Medeiros <ted.medeiros@ctis.com.br>
2019-06-14 17:23:18 -0300

    Iniciando integração com o back end

--- src/app/public/modules/episodios/components/episodio/episodio.component.ts
+++ src/app/public/modules/episodios/components/episodio/episodio.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, SecurityContext } from '@angular/core';
 import { Episodio } from '../../models/episodio.model';
 import { DomSanitizer } from '@angular/platform-browser';
+import { EpisodiosService } from '../../services/episodios.service';
 
 @Component({
   selector: 'app-episodio',
@@ -9,20 +10,27 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class EpisodioComponent implements OnInit {
 
-  episodio: Episodio = {
-    nome: "#32 - Balaio de Mala e Cuia",
-    categoria: "Viagens",
-    descricao: "Arruma-a-mala-ae, balaieiros e balaieiras! De Caicó ao Marrocos, o Balaio vai ganhar o mundo! Subam na nossa humilde rural e vamos meter o pé na estrada juntos.",
-    urlEpisodio: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/631346241&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-  }
+  episodios: Array<Episodio> = [];
 
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(
+    private episodiosService: EpisodiosService,
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit() {
+    this.getEpisodios();
+
   }
 
-  validUrl() {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(this.episodio.urlEpisodio);
+  private getEpisodios() {
+    this.episodiosService.getEpisodios().subscribe(episodios => {
+      this.episodios = episodios;
+    });
+  }
 
+
+
+  protected validUrl() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.episodios[0].urlEpisodio);
   }
 }
--- src/app/public/modules/episodios/services/episodios.service.ts
+++ src/app/public/modules/episodios/services/episodios.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from 'selenium-webdriver/http';
+import { Observable } from 'rxjs';
+import { Episodio } from '../models/episodio.model';
+import { HttpClient } from '@angular/common/http';
 
+const API = 'http://localhost:8080/' 
 @Injectable({
   providedIn: 'root'
 })
 export class EpisodiosService {
 
   constructor(private httpClient: HttpClient) { }
+
+
+  getEpisodios(): Observable<Array<Episodio>> {
+    
+    return this.httpClient.get<Array<Episodio>>(`${API}episodios`);
+  }
 }
